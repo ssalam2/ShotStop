ac968114fb9de2ea9f9fd39277c204a5dfac68fe
Sadaf Alam <ssalam2@ncsu.edu>
2025-08-14 19:48:50 -0400

    Kysely readded

--- src/api/server.js
+++ src/api/server.js
@@ -1,7 +1,7 @@
 // const app = require('./app')
 const express = require('express')
 const app = express()
-const [ sequelize ]  = require('./models/User')
+const [sequelize] = require('./models/User')
 
 app.listen(8080, () => console.log(sequelize.models.User))
 
